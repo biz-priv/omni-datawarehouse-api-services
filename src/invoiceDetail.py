c0322a0768c8340ebfea8f27efbef26045478656
kirankraleti <kiran.kraleti@bizcloudexperts.com>
2020-09-24 04:40:58 -0500

    invoice detail - code review

--- src/invoiceDetail.py
+++ src/invoiceDetail.py
@@ -1,9 +1,7 @@
 import psycopg2
 import logging
 import json
-import datetime
 import os
-import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -23,7 +21,6 @@ def handler(event, context):
 
         house_bill_nbr = event['query']['house_bill_nbr']
         records_list = []
-        invoice_records = {'updates':''}
 
         cur.execute(f"SELECT count(*) FROM public.shipment_info where house_bill_nbr = '{house_bill_nbr}'")
         con.commit()
@@ -36,14 +33,11 @@ def handler(event, context):
             cur.execute(f"select ar_invoice_receivables.file_nbr,shipment_info.house_bill_nbr, ar_invoice_receivables.revenue_stn,ar_invoice_receivables.invoice_nbr,ar_invoice_receivables.invoice_seq_nbr,customersb.name, customersc.name, ar_invoice_receivables.charge_cd_desc, ar_invoice_receivables.invoice_date, ar_invoice_receivables.due_date, ar_invoice_receivables.total from shipment_info join ar_invoice_receivables on shipment_info.source_system = ar_invoice_receivables.source_system  and shipment_info.file_nbr = ar_invoice_receivables.file_nbr left outer join public.customers customersb  on ar_invoice_receivables.source_system = customersb.source_system and trim(ar_invoice_receivables.bill_to_nbr) = trim(customersb.nbr) left outer join public.customers customersc on ar_invoice_receivables.source_system = customersc.source_system and trim(ar_invoice_receivables.bill_to_nbr) = trim(customersc.nbr) where house_bill_nbr = '{house_bill_nbr}'")
             con.commit()
             shipment_details = cur.fetchall()
-            charge_desc = charge_code(shipment_details)
-            invoices = convert_records(shipment_details[0], charge_desc)
+            invoices = convert_records(shipment_details[0], get_charge_code(shipment_details))
             records_list.append(invoices)
-            invoice_records = {'invoiceDetails':records_list}
+            invoice_records = {'invoiceDetails': records_list}
             logger.info("invoice details : {}".format(invoice_records))
-            
             return invoice_records
-            
         else:
             return "The provided house_bill_nbr does not exists"
 
@@ -64,16 +58,13 @@ def convert_records(shipment_charges,charge_desc):
         record["Invoice Date"] = modify_date(shipment_charges[8])
         record["Due Date"] = modify_date(shipment_charges[9])
         record["Charges"] = charge_desc
-
         return record
-
     except Exception as e:
         logging.exception("RecordsConversionError: {}".format(e))
         raise RecordsConversionError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
 
-def charge_code(shipment_details):
-    try:
-            
+def get_charge_code(shipment_details):
+    try: 
         charges_list =[]
         for charges in shipment_details:
             response = {}
@@ -83,9 +74,9 @@ def charge_code(shipment_details):
         return charges_list 
     
     except Exception as e:
-        logging.exception("chargeCodeError: {}".format(e))
-        raise chargeCodeError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
+        logging.exception("ChargeCodeError: {}".format(e))
+        raise ChargeCodeError(json.dumps({"httpStatus": 501, "message": InternalErrorMessage}))
 
 class HandlerError(Exception): pass
 class RecordsConversionError(Exception): pass
-class chargeCodeError(Exception): pass 
+class ChargeCodeError(Exception): pass 
