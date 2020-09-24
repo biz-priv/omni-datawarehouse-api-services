0a0b85f8438c1824a08b42fca85c7e4470bd85e4
manojk-at-739803869088 <manojk@bizcloudexperts.com>
2020-09-24 09:40:47 -0500

    local chenages for updateShipmentStatus lambda

--- src/updateShipmentStatus.py
+++ src/updateShipmentStatus.py
@@ -12,35 +12,36 @@ from src.common import dynamo_query
 def handler(event, context):
     try :
         logger.info("Event: {}".format(json.dumps(event)))
-
-        client = boto3.resource('dynamodb')
-        table = client.Table(os.environ['SHIPMENT_DETAILS_TABLE'])
-        record_status = 'True'
         response = dynamo_query(os.environ['SHIPMENT_DETAILS_TABLE'], os.environ['SHIPMENT_DETAILS_RECORDSTATUS_INDEX'], 
-                        'RecordStatus = :record_status', {":record_status": {"S": record_status}})
+                        'RecordStatus = :record_status', {":record_status": {"S": 'True'}})
         
         data = response['Items']
-        hb_values = [i['HouseBillNumber'] for i in data if 'HouseBillNumber' in i]
-        def convert(list): 
-            return tuple(list)
-        hb_for_sql = convert(hb_values)
-        # print("Count of all the hwb's from Dynamodb Table : ", hwb_length)
+        print("Data is :", data)
+        house_bill_nbr_list = [i['HouseBillNumber']['S'] for i in data if 'HouseBillNumber' in i]
+        print (house_bill_nbr_list)
+        
+        # house_bill_numbers = tuple(house_bill_nbr_list)
+        # logger.info("House Bill Numbers are : {}".format(house_bill_numbers))
 
         con=psycopg2.connect(dbname = os.environ['db_name'], host=os.environ['db_host'],
         port= os.environ['db_port'], user = os.environ['db_username'], password = os.environ['db_password'])
         con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) #psycopg2 extension to enable AUTOCOMMIT
         cur = con.cursor()
-        sql = (f"SELECT house_bill_nbr FROM public.api_shipment_info where house_bill_nbr IN {hb_for_sql}")
+        # format_strings = ','.join(['%s'] * len(house_bill_nbr_list))
+        # sql = (f"SELECT house_bill_nbr FROM public.api_shipment_info where house_bill_nbr IN (%s)" % format_strings, tuple(house_bill_nbr_list))
+        # print (sql)
+        sql = f"SELECT house_bill_nbr FROM public.api_shipment_info where house_bill_nbr IN %s" % (tuple(house_bill_nbr_list))
         cur.execute(sql)
         con.commit()
         #fetch the results from the slect statement executed above
         result = cur.fetchall()
         cur.close()
         con.close()
-        warehouse_hb_no = [housebill for (housebill,) in result]
-        print("These are the hwb from the warehouse : ", warehouse_hb_no)
+        print ("result:",result)
+        # warehouse_hb_no = [housebill for (housebill,) in result]
+        # print("These are the hwb from the warehouse : ", warehouse_hb_no)
         
-        for i in warehouse_hb_no:
+        for i in result:
             response = table.update_item(
                 TableName = os.environ['SHIPMENT_DETAILS_TABLE'],
                 Key={
@@ -55,7 +56,7 @@ def handler(event, context):
                     },
                 ReturnValues="UPDATED_NEW"
             )
-            # print("this is the updated response : ", response)
+            print("this is the updated response : ", response)
     except error as e:
         raise error({"Error": True,"message":str(e)})
 
