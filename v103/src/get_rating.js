4295dae963ed2cf51a3f3ace04f2555c2b262b74
Kazi Hasan Ali <kazihasanali@Kazis-MacBook-Pro.local>
2021-08-16 23:34:46 +0530

    4736 validations

--- v103/src/get_rating.js
+++ v103/src/get_rating.js
@@ -1,16 +1,19 @@
 const AWS = require("aws-sdk");
 const Joi = require("joi");
 const axios = require("axios");
-const { create, convert } = require("xmlbuilder2");
+const { convert } = require("xmlbuilder2");
 
+const replaceNull = Joi.allow(null).empty("").default(null);
 const CommodityInputValidation = {
-  CommodityPieces: Joi.number().integer(),
-  CommodityPieceType: Joi.string(),
-  CommodityWeight: Joi.number().integer(),
-  CommodityLength: Joi.number().integer(),
-  CommodityWidth: Joi.number().integer(),
-  CommodityHeight: Joi.number().integer(),
-  CommodityHazmat: Joi.number().integer(),
+  CommodityClass: Joi.number().precision(2).concat(replaceNull),
+  CommodityPieces: Joi.number().integer().concat(replaceNull),
+  CommodityPieceType: Joi.string().concat(replaceNull),
+  CommodityWeightPerPiece: Joi.number().integer().concat(replaceNull),
+  CommodityWeight: Joi.number().integer().concat(replaceNull),
+  CommodityLength: Joi.number().integer().concat(replaceNull),
+  CommodityWidth: Joi.number().integer().concat(replaceNull),
+  CommodityHeight: Joi.number().integer().concat(replaceNull),
+  CommodityHazmat: Joi.string().concat(replaceNull),
 };
 const eventValidation = Joi.object().keys({
   RatingInput: Joi.object()
@@ -25,15 +28,14 @@ const eventValidation = Joi.object().keys({
       DestinationState: Joi.string(),
       DestinationZip: Joi.number().integer().required(),
       ShipmentTerms: Joi.string(),
-      PickupDate: Joi.date().required(),
-      PickupTime: Joi.date().required(),
-      PickupLocationCloseTime: Joi.date().required(),
+      PickupDate: Joi.date().iso().greater("now").required(),
+      PickupTime: Joi.date().iso().greater("now").required(),
+      PickupLocationCloseTime: Joi.date().iso().greater("now").required(),
     })
     .required(),
   CommodityInput: Joi.object()
     .keys({
       CommodityInput: Joi.alternatives(
-        Joi.string(),
         Joi.object().keys(CommodityInputValidation),
         Joi.array().items(CommodityInputValidation)
       ).required(),
@@ -50,15 +52,30 @@ module.exports.handler = async (event, context, callback) => {
     return errorMsg(400, "Please provide all required fields.", error.details);
   }
   const apiKey = event.headers["x-api-key"];
-  const eventBody = event.body;
+  const eventBody = value;
   eventBody.RatingInput.LiabilityType = LiabilityType;
   eventBody.RatingInput.WebTrakUserID = WebTrakUserID;
+  eventBody.RatingInput.PickupDate =
+    event.body.RatingInput.PickupDate.toString();
+  eventBody.RatingInput.PickupTime =
+    event.body.RatingInput.PickupTime.toString();
+  eventBody.RatingInput.PickupLocationCloseTime =
+    event.body.RatingInput.PickupLocationCloseTime.toString();
   try {
-    const customerId = await getCustomerId(apiKey);
-    if (customerId == null) {
+    const customerData = await getCustomerId(apiKey);
+    if (customerData == null) {
       return errorMsg(400, "Api key validation error");
+    } else if (
+      !customerData.hasOwnProperty("WebTrackId") ||
+      customerData.WebTrackId == null
+    ) {
+      return errorMsg(400, "No valid WebTrackId");
     }
+    eventBody.RatingInput.WebTrackId = customerData.WebTrackId;
+
     const postData = makeJsonToXml(eventBody);
+    console.log(postData);
+    // return {};
     const dataResponse = await getRating(postData);
     const dataObj = makeXmlToJson(dataResponse);
     return dataObj;
@@ -111,12 +128,8 @@ function getCustomerId(ApiKey) {
     };
     try {
       const response = await documentClient.scan(params).promise();
-      if (
-        response.Items &&
-        response.Items.length > 0 &&
-        response.Items[0].CustomerID
-      ) {
-        resolve(response.Items[0].CustomerID);
+      if (response.Items && response.Items.length > 0) {
+        resolve(response.Items[0]);
       } else {
         resolve(null);
       }
