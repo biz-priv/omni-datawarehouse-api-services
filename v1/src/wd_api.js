4ef517dd42eb62e1e653b1e660de5439e93bda68
kazibizcloud <kazi.ali@bizcloudexperts.com>
2021-12-13 18:01:48 +0530

    wd api completed

--- v1/src/wd_api.js
+++ v1/src/wd_api.js
@@ -3,67 +3,45 @@ const Joi = require("joi");
 const axios = require("axios");
 const { convert } = require("xmlbuilder2");
 const pgp = require("pg-promise");
+const payload = require("../../Helpers/wd_payload.json");
 
 module.exports.handler = async (event, context, callback) => {
   try {
-    const dbUser = process.env.USER;
-    const dbPassword = process.env.PASS;
-    // const dbHost = process.env.HOST;
-    const dbHost = "omni-dw-prod.cnimhrgrtodg.us-east-1.redshift.amazonaws.com"; //process.env.HOST;
-    const dbPort = process.env.PORT;
-    const dbName = process.env.DBNAME;
+    /**
+     * Get data from db
+     */
+    const shipmentData = await getDataFromDB();
 
-    const dbc = pgp({ capSQL: true });
-    console.log(`Opening connection to: ${dbName}, host is: ${dbHost}`);
+    /**
+     * Check shipment data in dynamo db
+     */
+    const newData = await checkStatus(shipmentData);
 
-    const connectionString = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
-    const connections = dbc(connectionString);
-    // console.log("connections", connections);
-    const query = `select
-    a.file_nbr ,a.house_bill_nbr ,a.handling_stn ,a.controlling_stn ,a.chrg_wght_lbs ,a.chrg_wght_kgs ,
-    case b.order_status
-    when 'PUP' then 'AF'
-    when 'AAP' then 'AV / X1'
-    when 'DPO' then 'CD'
-    when 'COB' then 'AN'
-    when 'DEL'then 'D1'
-    end order_Status,
-    case b.order_status
-    when 'PUP' then 'Pick Up Confirmed'
-    when 'AAP' then 'Actual Arrival at Destination Port'
-    when 'DPO' then 'Actual departure Destination Port'
-    when 'COB' then 'Confirmed On Board'
-    when 'DEL'then 'Delivered - No Exception'
-    end order_Status_Desc,
-    b.order_status_desc ,b.event_date_utc ,c.ref_nbr
-    from
-    shipment_info a
-    left outer join shipment_milestone b
-    on a.file_nbr = b.file_nbr
-    and a.source_system = b.source_system
-    left outer join shipment_ref c
-    on a.source_system = c.source_system
-    and a.file_nbr = c.file_nbr
-    and c.ref_typeid = 'SID'
-    where a.bill_to_nbr = '17833'`;
+    await Promise.all(
+      newData.map(async (item) => {
+        /**
+         * Make Json to Xml payload
+         */
+        const xmlPayload = makeJsonToXml(payload, item);
 
-    const result = await connections.query(query);
-    console.log("result", result);
-    // const demoResult = [
-    //   {
-    //     file_nbr: "3661515",
-    //     house_bill_nbr: "3651511",
-    //     handling_stn: "LAX",
-    //     controlling_stn: "LGB",
-    //     chrg_wght_lbs: "192.00",
-    //     chrg_wght_kgs: "87.50",
-    //     order_status: null,
-    //     order_status_desc: null,
-    //     event_date_utc: null,
-    //     ref_nbr: null,
-    //   },
-    // ];
-    return {};
+        /**
+         * Get response from WD api
+         */
+        const xmlResponse = await getXmlResponse(xmlPayload);
+
+        /**
+         * make Xml to Json response
+         */
+        const refTransmissionNo = makeXmlToJson(xmlResponse);
+
+        /**
+         * Update shipment data to dynamo db
+         */
+        await updateStatus(item, xmlPayload, xmlResponse, refTransmissionNo);
+      })
+    );
+
+    return "Completed";
   } catch (error) {
     return callback(
       response(
@@ -74,105 +52,162 @@ module.exports.handler = async (event, context, callback) => {
   }
 };
 
-function makeJsonToXml(data) {
-  return convert({
-    "soap12:Envelope": {
-      "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-      "@xmlns:xsd": "http://www.w3.org/2001/XMLSchema",
-      "@xmlns:soap12": "http://www.w3.org/2003/05/soap-envelope",
-      "soap12:Body": {
-        GetRating: {
-          "@xmlns": "http://tempuri.org/",
-          RatingParam: data,
-        },
-      },
-    },
-  });
-}
-
-function makeXmlToJson(data) {
+async function getDataFromDB(data = null) {
   try {
-    let obj = convert(data, { format: "object" });
-    if (
-      obj["soap:Envelope"][
-        "soap:Body"
-      ].GetRatingResponse.GetRatingResult.hasOwnProperty("RatingOutput")
-    ) {
-      const modifiedObj =
-        obj["soap:Envelope"]["soap:Body"].GetRatingResponse.GetRatingResult
-          .RatingOutput;
-      console.log("modifiedObj", modifiedObj);
+    const dbUser = process.env.USER;
+    const dbPassword = process.env.PASS;
+    const dbHost = process.env.HOST_URL;
+    const dbPort = process.env.PORT;
+    const dbName = process.env.WD_DBNAME;
 
-      if (isArray(modifiedObj)) {
-        return modifiedObj.map((e) => {
-          if (isEmpty(e.Message)) {
-            e.Message = "";
-          }
-          let AccessorialOutput = null;
-          if (
-            e.AccessorialOutput &&
-            e.AccessorialOutput.AccessorialOutput &&
-            e.AccessorialOutput.AccessorialOutput[0] == null
-          ) {
-            const list = [];
-            list.push(e.AccessorialOutput.AccessorialOutput);
-            AccessorialOutput = list;
-          } else {
-            AccessorialOutput = e.AccessorialOutput.AccessorialOutput;
-          }
+    const dbc = pgp({ capSQL: true });
+    const connectionString = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+    const connections = dbc(connectionString);
+    const query = `select distinct
+    a.file_nbr ,a.house_bill_nbr ,
+    a.handling_stn ,a.controlling_stn ,a.chrg_wght_lbs ,a.chrg_wght_kgs ,pieces,
+    case b.order_status
+    when 'PUP' then 'AF'
+    when 'COB' then 'AN'
+    when 'DEL'then 'D1'
+    end order_Status,
+    case b.order_status
+    when 'PUP' then 'Pick Up Confirmed'
+    when 'COB' then 'Confirmed On Board'
+    when 'DEL'then 'Delivered - No Exception'
+    end order_Status_Desc,
+    case when b.order_status in ('PUP','COB','DEL') then b.event_date_utc else null end as Event_Date_utc,
+    case when b.order_status in ('PUP','COB') then A.ORIGIN_PORT_IATA
+    when b.order_status in ('DEL') then A.DESTINATION_PORT_IATA
+    else '' end as event_city,
+    case when b.order_status in ('PUP','COB','DEL') then  'US' else '' end as Event_country,
+    c.ref_nbr
+        from
+        shipment_info a
+        left outer join shipment_milestone b
+        on a.file_nbr = b.file_nbr
+        and a.source_system = b.source_system
+        left outer join
+        (select distinct source_system ,file_nbr ,ref_nbr from shipment_ref where ref_typeid = 'REF') c
+        on a.source_system = c.source_system
+        and a.file_nbr = c.file_nbr
+        where a.bill_to_nbr = '17833'
+        and b.order_status in ('PUP','COB','DEL')`;
 
-          return {
-            ServiceLevelID: e.ServiceLevelID,
-            StandardTotalRate: e.StandardTotalRate,
-            StandardFreightCharge: e.StandardFreightCharge,
-            AccessorialOutput:
-              AccessorialOutput == null ? "" : AccessorialOutput,
-            Message: e.Message,
-          };
-        });
-      } else {
-        if (isEmpty(modifiedObj.Message)) {
-          modifiedObj.Message = "";
-        } else if (modifiedObj.Message.search("WebTrakUserID") != -1) {
-          throw "Internal error message";
-        }
-        let AccessorialOutput = null;
-        if (
-          modifiedObj.AccessorialOutput &&
-          modifiedObj.AccessorialOutput.AccessorialOutput &&
-          modifiedObj.AccessorialOutput.AccessorialOutput[0] == null
-        ) {
-          const list = [];
-          list.push(modifiedObj.AccessorialOutput.AccessorialOutput);
-          AccessorialOutput = list;
-        } else {
-          AccessorialOutput = modifiedObj.AccessorialOutput.AccessorialOutput;
-        }
-        return [
-          {
-            ServiceLevelID: modifiedObj.ServiceLevelID,
-            StandardTotalRate: modifiedObj.StandardTotalRate,
-            Message: modifiedObj.Message,
-            StandardFreightCharge: modifiedObj.hasOwnProperty(
-              "StandardFreightCharge"
-            )
-              ? modifiedObj.StandardFreightCharge
-              : "",
-            AccessorialOutput:
-              AccessorialOutput == null ? "" : AccessorialOutput,
-          },
-        ];
-      }
-    } else {
-      throw "Rate not found.";
+    const result = await connections.query(query);
+    if (!result || result.length == 0) {
+      throw "No data found.";
     }
-  } catch (e) {
-    throw e.hasOwnProperty("message") ? e.message : e;
+    return result;
+  } catch (error) {
+    throw "No data found.";
   }
 }
 
-function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
+function makeJsonToXml(payload, inputData) {
+  payload["soapenv:Envelope"]["soapenv:Header"]["wsse:Security"][
+    "wsse:UsernameToken"
+  ]["wsse:Username"] = process.env.WD_API_USERNAME;
+  payload["soapenv:Envelope"]["soapenv:Header"]["wsse:Security"][
+    "wsse:UsernameToken"
+  ]["wsse:Password"]["#"] = process.env.WD_API_PASSWORD;
+
+  /**
+   * TransmissionHeader values
+   */
+  let transHeader =
+    payload["soapenv:Envelope"]["soapenv:Body"]["tran:publish"][
+      "otm:Transmission"
+    ]["otm:TransmissionHeader"];
+
+  /**
+   * TransmissionBody values
+   */
+  let transBody =
+    payload["soapenv:Envelope"]["soapenv:Body"]["tran:publish"][
+      "otm:Transmission"
+    ]["otm:TransmissionBody"]["otm:GLogXMLElement"]["otm:ShipmentStatus"];
+  transBody["otm:IntSavedQuery"]["otm:IntSavedQueryArg"][0]["otm:ArgValue"] =
+    inputData.house_bill_nbr;
+
+  transBody["otm:IntSavedQuery"]["otm:IntSavedQueryArg"][1]["otm:ArgValue"] =
+    inputData.house_bill_nbr;
+
+  transBody["otm:ShipmentRefnum"][0]["otm:ShipmentRefnumValue"] =
+    "H" + inputData.ref_nbr;
+  transBody["otm:ShipmentRefnum"][1]["otm:ShipmentRefnumValue"] =
+    inputData.chrg_wght_kgs;
+
+  transBody["otm:ShipmentRefnum"][2]["otm:ShipmentRefnumValue"] =
+    inputData.pieces;
+  transBody["otm:WeightVolume"]["otm:Weight"]["otm:WeightValue"] =
+    inputData.chrg_wght_kgs;
+
+  transBody["otm:EventDt"]["otm:GLogDate"] = formatDate(
+    inputData.event_date_utc
+  );
+
+  transBody["otm:SSStop"]["otm:SSLocation"]["otm:EventCity"] =
+    inputData.event_city;
+  transBody["otm:SSStop"]["otm:SSLocation"]["otm:EventCountry"] =
+    inputData.event_country;
+
+  transBody["otm:TrackingNumber"] = "H" + inputData.ref_nbr;
+
+  transBody["otm:ShipmentGid"]["otm:Gid"]["otm:Xid"] = inputData.house_bill_nbr;
+
+  /**
+   * set the header and body data
+   */
+  payload["soapenv:Envelope"]["soapenv:Body"]["tran:publish"][
+    "otm:Transmission"
+  ]["otm:TransmissionHeader"] = transHeader;
+  payload["soapenv:Envelope"]["soapenv:Body"]["tran:publish"][
+    "otm:Transmission"
+  ]["otm:TransmissionBody"]["otm:GLogXMLElement"]["otm:ShipmentStatus"] =
+    transBody;
+  return convert(payload);
+}
+
+async function getXmlResponse(postData) {
+  try {
+    const res = await axios.post(process.env.WD_API, postData, {
+      headers: {
+        Accept: "text/xml",
+        "Content-Type": "text/xml; charset=utf-8",
+      },
+    });
+    return {
+      xml_response: res.data,
+      status_code: res.status,
+      status: res.status == 200 ? "success" : "failed",
+    };
+  } catch (e) {}
+}
+
+function makeXmlToJson(xmlResponse) {
+  try {
+    const obj = convert(xmlResponse.xml_response, { format: "object" });
+    return obj["S:Envelope"]["S:Body"]["publishResponse"][
+      "otm:TransmissionAck"
+    ]["otm:EchoedTransmissionHeader"]["otm:TransmissionHeader"][
+      "otm:ReferenceTransmissionNo"
+    ];
+  } catch (error) {
+    return null;
+  }
+}
+
+function formatDate(dateObj) {
+  var date = new Date(dateObj);
+  return (
+    date.getFullYear() +
+    ("00" + (date.getMonth() + 1)).slice(-2) +
+    ("00" + date.getDate()).slice(-2) +
+    ("00" + date.getHours()).slice(-2) +
+    ("00" + date.getMinutes()).slice(-2) +
+    ("00" + date.getSeconds()).slice(-2)
+  );
 }
 
 function response(code, message) {
@@ -182,20 +217,69 @@ function response(code, message) {
   });
 }
 
-async function getRating(postData) {
+async function updateStatus(
+  record,
+  xmlPayload,
+  xmlResponse,
+  refTransmissionNo
+) {
+  let documentClient = new AWS.DynamoDB.DocumentClient({
+    region: process.env.DEFAULT_AWS,
+  });
+  const params = {
+    TableName: process.env.WD_SHIPMENT_STATUS_TABLE,
+    Item: {
+      ...record,
+      id: record.file_nbr.toString() + record.order_status,
+      ReferenceTransmissionNo: refTransmissionNo,
+      xml_payload: xmlPayload,
+      ...xmlResponse,
+      status:
+        refTransmissionNo == -1 || refTransmissionNo == null
+          ? "failed"
+          : xmlResponse.status,
+      event_date_utc: new Date(record.event_date_utc).toLocaleString(),
+      created_at: new Date().toLocaleString(),
+    },
+  };
   try {
-    const res = await axios.post(process.env.RATING_API, postData, {
-      headers: {
-        Accept: "text/xml",
-        "Content-Type": "text/xml; charset=utf-8",
-      },
+    await documentClient.put(params).promise();
+  } catch (e) {}
+}
+
+async function checkStatus(shipmentData) {
+  try {
+    const documentClient = new AWS.DynamoDB.DocumentClient({
+      region: process.env.REGION,
     });
-    if (res.status == 200) {
-      return res.data;
+    let idList = {};
+    shipmentData.map((e, i) => {
+      idList[":" + i] = e.file_nbr.toString() + e.order_status;
+    });
+    const idMaping = Object.keys(idList);
+    const params = {
+      TableName: process.env.WD_SHIPMENT_STATUS_TABLE,
+      FilterExpression: "#id IN (" + idMaping.join(",") + ")",
+      ExpressionAttributeNames: { "#id": "id" },
+      ExpressionAttributeValues: idList,
+    };
+    const res = await documentClient.scan(params).promise();
+    let newData = [];
+    if (res && res.Count != 0) {
+      const oldIds = res.Items.map(
+        (e) => e.file_nbr.toString() + e.order_status
+      );
+      newData = shipmentData.filter((e) => {
+        let idKey = e.file_nbr.toString() + e.order_status;
+        if (oldIds.indexOf(idKey) != -1) {
+          return false;
+        } else {
+          return true;
+        }
+      });
     } else {
-      throw e.response.statusText;
+      newData = shipmentData;
     }
-  } catch (e) {
-    throw e.hasOwnProperty("response") ? "Request failed" : e;
-  }
+    return newData;
+  } catch (e) {}
 }
