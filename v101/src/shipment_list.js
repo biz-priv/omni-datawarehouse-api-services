4d76400d0987f51e8a3c0b72927dc16a9ffeb2f6
Sujit <85719346+sujit-bizcloudexperts@users.noreply.github.com>
2022-08-02 17:52:43 +0530

    updated default

--- v101/src/shipment_list.js
+++ v101/src/shipment_list.js
@@ -21,8 +21,8 @@ module.exports.handler = async (event, context, callback) => {
       },
     });
     let totalCount = 0;
-    let page = _.get(event, 'queryStringParameters.page')
-    let size = _.get(event, 'queryStringParameters.size') 
+    let page = _.get(event, 'queryStringParameters.page') || 1
+    let size = _.get(event, 'queryStringParameters.size') || 10
   
     if (!customerID.error) {
       if (customerID.length) {
