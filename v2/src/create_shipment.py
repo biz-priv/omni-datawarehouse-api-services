7332a757f495a10c6dcfcd013358d1b28265322d
alexa-kattampally <alexa.kattampally@bizcloudexperts.com>
2022-10-26 12:07:51 -0500

    lowercase changes

--- v2/src/create_shipment.py
+++ v2/src/create_shipment.py
@@ -321,6 +321,8 @@ def get_shipment_line_list(data_obj):
                 data_obj["shipmentLines"])
             for i in temp_shipment_line_list:
                 i['Hazmat'] = str(i['Hazmat']).lower()
+                i['WeightUOMV3'] = str(i['WeightUOMV3']).lower()
+                i['DimUOMV3'] = str(i['DimUOMV3']).lower()
             def shipment_line_list_item(x): return 'NewShipmentDimLineV3'
             shipment_line_list = dicttoxml.dicttoxml(temp_shipment_line_list,
                                                         attr_type=False, custom_root='ShipmentLineList', item_func=shipment_line_list_item)
