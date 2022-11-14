2152413874f585276e0146557e35c423fecb5603
alexa-kattampally <alexa.kattampally@bizcloudexperts.com>
2022-11-14 10:40:25 -0600

    customerNumber validation

--- v2/src/get_rate.js
+++ v2/src/get_rate.js
@@ -40,6 +40,9 @@ module.exports.handler = async (event, context, callback) => {
   ) {
     valError =
       "shipperZip, consigneeZip, and pickupTime are required fields. Please ensure you are sending all 3 of these values.";
+  } else if (!('customerNumber' in body.shipmentRateRequest)){
+    error = 'customerNumber is a required field for this request.'
+    
   } else {
     reqFields.shipperZip = body.shipmentRateRequest.shipperZip;
     reqFields.consigneeZip = body.shipmentRateRequest.consigneeZip;
