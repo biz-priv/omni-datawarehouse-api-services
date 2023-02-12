4b0e1d17c40db04872eeba1fe0f1e485708714d9
vs002666 <ASHISH_AKSHANTAL@neimanmarcus.com>
2023-02-12 14:57:27 -0600

    added changes in custom-authorizer

--- v2/src/custom_authorizer.py
+++ v2/src/custom_authorizer.py
@@ -57,7 +57,7 @@ def handler(event, context):
             {"httpStatus": 400, "message": "API Key not passed."})) from api_key_error
 
     # Validating params only for the GET APIs
-    if "/create/shipment" not in event["methodArn"] and "/rate" not in event["methodArn"] and "/addDocument" not in event["methodArn"]:
+    if "/create/shipment" not in event["methodArn"] and "/rate" not in event["methodArn"] and "/addDocument" not in event["methodArn"] and "/getDocument" not in event["methodArn"]:
         validation_response = validate_input(params)
         if validation_response["status"] == "error":
             return generate_policy(None, 'Deny', event["methodArn"], None, validation_response["message"])
@@ -80,7 +80,7 @@ def handler(event, context):
         return generate_policy(POLICY_ID, 'Allow', event["methodArn"], customer_id)
     elif "shipment/addDocument" in event["methodArn"]:
         return generate_policy(POLICY_ID, 'Allow', event["methodArn"], customer_id)
-    elif "/shipment/getDocument" in event["methodArn"]:
+    elif "shipment/getDocument" in event["methodArn"]:
         return generate_policy(POLICY_ID, 'Allow', event["methodArn"], customer_id)
 
     else:
