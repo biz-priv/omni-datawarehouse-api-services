858a936894163f9a98e268b5e8e6dfcf9d527369
Dsish <debasish@yeasitech.com>
2023-02-15 11:31:45 +0530

    bugfix testing

--- v2/src/get_document.js
+++ v2/src/get_document.js
@@ -67,17 +67,15 @@ const fileNumberSchema = Joi.object({
 });
 
 module.exports.handler = async (event) => {
+  console.log("Event", event);
+  return {};
   try {
-    console.log("Event", event);
     const eventParams = event.queryStringParameters;
     // const xApiKey = event.headers;
     // const eventParams = event;
     let response;
     let newResponse;
-    console.log(
-      "/websli/api/url",
-      process.env.GET_DOCUMENT_API
-    );
+    console.log("/websli/api/url", process.env.GET_DOCUMENT_API);
 
     //2. valiadte the params
     if (!eventParams) {
