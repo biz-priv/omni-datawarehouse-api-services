6d826e5364dc5829f4fc08c1ba5e470c88f95159
Madhava mvs <VenkataSaiMadhava_Matta@neimanmarcus.com>
2023-08-22 18:02:05 +0530

    updated console logs for dev env

--- v201/src/get_document.js
+++ v201/src/get_document.js
@@ -166,11 +166,9 @@ module.exports.handler = async (event, context, callback) => {
         ':ApiKey': apiKey 
       }
     };
-    console.log(params)
     const data = await ddb.query(params).promise();
     const websliKey = get(data, "Items[0].websli_key", "")
-    console.log(data)
-    console.log("api key for calling websli", data.Items[0].websli_key)
+    console.log("websli api key record in token validator", data)
   
 
     // await getDataWithoutGateway(eventParams, parameterString, searchType);
@@ -241,7 +239,7 @@ async function getData(eventParams, parameterString, searchType, apiKey) {
     console.log("data", getDocumentData);
     return getDocumentData;
   } catch (error) {
-    console.log("error", error);
+    console.log("error while calling websli endpoint: ", error);
     throw error;
   }
 }
