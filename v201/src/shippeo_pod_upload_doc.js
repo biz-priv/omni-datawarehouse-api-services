9ef06320cea80156f5a31a1b17f73d526dcacec6
Jahir <jahir.uddin@bizcloudexperts.com>
2023-08-31 20:40:23 +0530

    get file from websli

--- v201/src/shippeo_pod_upload_doc.js
+++ v201/src/shippeo_pod_upload_doc.js
@@ -9,6 +9,7 @@ const {
 	LOG_TABLE,
 	SHIPPEO_GET_TOKEN_URL,
 	SHIPPEO_GET_DOC_API_KEY,
+	WT_WEBSLI_API_URL,
 } = process.env;
 const { get } = require("lodash");
 const axios = require("axios");
@@ -18,6 +19,8 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const sns = new AWS.SNS();
 const sqs = new AWS.SQS();
 const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
 
 module.exports.handler = async (event, context, callback) => {
 	let body;
@@ -67,20 +70,16 @@ module.exports.handler = async (event, context, callback) => {
 					xApiKey
 				);
 				// const xApiKey = "fIZpXhfGKQ42h6zIs7EUetiJd9yiAui7LlZxbkFh";
-				const getDocumentRes = await getDocument({ url: getDocUrl, xApiKey });
-				const docs = get(getDocumentRes, "getDocumentResponse.documents", []);
+				// const getDocumentRes = await getDocument({ url: getDocUrl, xApiKey });
+				// const docs = get(getDocumentRes, "getDocumentResponse.documents", []);
 
 				const uploadToUrl = `${SHIPPEO_UPLOAD_DOC_URL}/${6986204}/files`;
-				await uploadDocs({ docs, uploadToUrl, token });
-				console.log(
-					"🚀 ~ file: shippeo_pod_upload_doc.js:24 ~ module.exports.handler= ~ docs:",
-					docs
-				);
+				await uploadDocs({ uploadToUrl, token, houseBillNo });
 			})
 		);
 	} catch (error) {
 		console.error("Error : \n", error);
-		return
+		return;
 		try {
 			const params = {
 				Subject: "Error on shippeo-pod-upload-doc lambda",
@@ -139,41 +138,155 @@ const getDocument = async ({ url, xApiKey }) => {
 	return data;
 };
 
-const uploadDocs = async ({ docs, uploadToUrl, token }) => {
+const uploadDocs = async ({ uploadToUrl, token, houseBillNo }) => {
 	const formData = new FormData();
-	docs.forEach(async (doc) => {
-		const { data: fileContent } = await axios.get(get(doc, "url"), {
-			responseType: "arraybuffer",
-		});
-		const bufferData = Buffer.from(fileContent);
-		fs.writeFileSync(get(doc, "filename"), bufferData);
-		formData.append(
-			"attachments[]",
-			fs.createReadStream(get(doc, "filename")),
-			{
-				filename: get(doc, "filename"),
-			}
-		);
-	});
+	// docs.forEach(async (doc) => {
+	// 	const { data: fileContent } = await axios.get(get(doc, "url"), {
+	// 		responseType: "arraybuffer",
+	// 	});
+	// 	console.log(
+	// 		"🚀 ~ file: shippeo_pod_upload_doc.js:146 ~ docs.forEach ~ fileContent:",
+	// 		typeof fileContent
+	// 	);
+	// 	const fileName = get(doc, "filename");
+	// 	console.log(
+	// 		"🚀 ~ file: shippeo_pod_upload_doc.js:149 ~ docs.forEach ~ fileName:",
+	// 		fileName
+	// 	);
+	// 	const bufferData = Buffer.from(fileContent);
+	// 	const base64String = bufferData.toString("base64");
+	// 	fs.writeFileSync(fileName, base64String);
+	// 	formData.append("attachments[]", fileContent);
+	// 	// formData.append("attachments[]", fs.createReadStream(fileName));
+	// });
 
-	const data = await axios.post(uploadToUrl, formData, {
-		headers: {
-			...formData.getHeaders(),
-			maxBodyLength: Infinity,
-			Authorization: `Bearer ${token}`,
-		},
+	const { b64str, api_status_code, Res } = await callWtRestApi(houseBillNo);
+	const filePath = `/tmp/${houseBillNo}.pdf`;
+	// createDirectoryIfNotExists(tempPath);
+	// const filePath = path.join(__dirname, `${houseBillNo}.pdf`);
+	// const filePath = path.join(tempPath, `${houseBillNo}.pdf`);
+	// await convertBase64ToPdf(b64str, filePath);
+	const fileData = Buffer.from(b64str, "base64");
+	console.log(
+		"🚀 ~ file: shippeo_pod_upload_doc.js:169 ~ uploadDocs ~ fileData:",
+		filePath
+	);
+	fs.writeFileSync(filePath, fileData);
+
+	// const curlCommand = `curl --location http://api-edi.shippeo.com/api/orders/EDIReference/6986204/files --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTMzODMwNzYsImV4cCI6MTY5NDU5MjY3NiwiaGFzaGlkIjoiOTJFOTNNRTIifQ.aHjgHl8ZlNR1Qg7diFWu27ECoZSJ3hzN5WvwvvOQx2eu7vdVk_W5n4R11m0XR9NeUMVAIF-8yvHnPLFhnZ1eN25h7HwNcPNN_IJLAU874S-X4eGBt44_uJS19jMnG0NBZAwIVw1BlgPVGSUtYxb0U90P3usqoPzQU89gjiivXSpeny3oa40WLMBq_iSl9aCKAZHtwfF-O0eAWKxNJUyxD26r-NM0OBPOqdHm0JHUTFeeOlCYfqSufzSGTJ72NiWTW6PVj0sekTKv445AjC7NipJnq_JiIBUbwltq9DwRg4sXT8II8bEi9sSeYKXR9y_ERQSkSSUK_ITJ3uHXNzoPJQ3BOui8TRRE8f30Mo9IfzgFuB7CSMAf6xHxFaaidvznqWDg6ECUqZfhLDuKTJZ0DG773OgMCBoBehawwURRO9qrl6ObwTIHHnbUzP3q9yjOK6GxNfvW7rEb8dHG3sOtCtCcLUBVoml9UQ6tc5TffYXQYUVx4xmwfTJcFV9yk7CTDDtgfCFn32dP5fwc6bvoxKKdWPeV6XwKRfeXs-GgXe_1RldQ0i-wvKIpAptlWheqYUUyVD7IItnDzJ_9D5OKwKxPrACebkFWYhbzLXS4IhcxWp8-BOMvzZrk3rikOCiI9V57SICQqN5pAYQr8jA-fLrTsLR9ldu4_PUNuzN3Jng' --form 'attachments[]=@"D:\\Work\\omni\\omni-datawarehouse-api-services\\v201\\src\\6466813.pdf"'`;
+
+	// exec(curlCommand, (error, stdout, stderr) => {
+	// 	if (error) {
+	// 		console.error(`Error executing cURL command: ${error}`);
+	// 		return;
+	// 	}
+	// 	// Handle the output
+	// 	console.log("cURL command output:", stdout);
+	// });
+
+	const file = fs.createReadStream(filePath, {
+		encoding: "base64",
 	});
+	console.log(
+		"🚀 ~ file: shippeo_pod_upload_doc.js:169 ~ uploadDocs ~ file:",
+		file
+	);
+
+	formData.append("attachments[]", file);
+	const fileStat = fs.statSync(filePath);
+	const contentLength = String(get(fileStat, "size", 0));
+	console.log(
+		"🚀 ~ file: shippeo_pod_upload_doc.js:173 ~ uploadDocs ~ contentLength:",
+		contentLength
+	);
+	try {
+		const config = {
+			method: "post",
+			maxBodyLength: Infinity,
+			url: uploadToUrl,
+			headers: {
+				...formData.getHeaders(),
+				maxBodyLength: Infinity,
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "multipart/form-data",
+				"Content-Length": contentLength,
+			},
+			data: formData,
+		};
+		console.log(
+			"🚀 ~ file: shippeo_pod_upload_doc.js:188 ~ uploadDocs ~ config:",
+			JSON.stringify(config)
+		);
+		const response = await axios.request(config);
+		// console.log(
+		// 	"🚀 ~ file: shippeo_pod_upload_doc.js:182 ~ uploadDocs ~ response:",
+		// 	response
+		// );
+		console.log("Response Data:", response.data);
+		console.log("Status Code:", response.status);
+	} catch (error) {
+		if (error.response) {
+			// The request was made and the server responded with a non-2xx status code
+			console.log("Status Code:", error.response.status);
+			console.log("Response Data:", JSON.stringify(error.response.data)); // This contains the error response data
+			console.log("Response Headers:", error.response.headers);
+		} else if (error.request) {
+			// The request was made but no response was received
+			console.error("No response received:", error.request);
+		} else {
+			// An error occurred while setting up the request
+			console.error("Error:", error.message);
+		}
+	}
 	// if(data.status === 200){
 	// 	await insertLog();
 	// }
-	console.log(
-		"🚀 ~ file: shippeo_pod_upload_doc.js:89 ~ uploadDocs ~ data:",
-		data
-	);
-	docs.forEach(async (doc) => {
-		fs.unlinkSync(get(doc, "filename"));
-	});
+
+	// docs.forEach(async (doc) => {
+	// 	fs.unlinkSync(get(doc, "filename"));
+	// });
 };
+
+function createDirectoryIfNotExists(directoryPath) {
+	if (!fs.existsSync(directoryPath)) {
+		fs.mkdirSync(directoryPath, { recursive: true });
+		console.log(`Directory '${directoryPath}' created.`);
+	} else {
+		console.log(`Directory '${directoryPath}' already exists.`);
+	}
+}
+
+function convertBase64ToPdf(base64String, filePath) {
+	return new Promise((resolve, reject) => {
+		const fileData = Buffer.from(base64String, "base64");
+		fs.writeFileSync(filePath, fileData, function (err) {
+			if (err) {
+				console.error("Error converting Base64 to PDF:", err);
+				reject(err);
+			}
+			resolve();
+		});
+	});
+}
+
+async function callWtRestApi(housebill) {
+	try {
+		const url = `${process.env.WT_WEBSLI_API_URL}/9980f7b9eaffb71ce2f86734dae062/housebill=${housebill}/doctype=HOUSEBILL`;
+
+		const response = await axios.get(url);
+		return {
+			b64str: response.data.wtDocs.wtDoc[0].b64str,
+			api_status_code: response.status,
+			Res: response,
+		};
+	} catch (error) {
+		console.error(
+			`Error calling WT REST API for housebill ${housebill}:`,
+			error
+		);
+		return "error";
+	}
+}
 
 // const
 
