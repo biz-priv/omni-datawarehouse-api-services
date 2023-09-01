400328b65f8d11a6e03d7fa03fb89ab2d551642f
Jahir <jahir.uddin@bizcloudexperts.com>
2023-09-01 21:15:53 +0530

    shippeo update

--- v201/src/shippeo_pod_upload_doc.js
+++ v201/src/shippeo_pod_upload_doc.js
@@ -21,7 +21,7 @@ const sqs = new AWS.SQS();
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
-const http = require("follow-redirects").http;
+const request = require("request");
 
 module.exports.handler = async (event, context, callback) => {
 	let body;
@@ -162,23 +162,25 @@ const uploadDocs = async ({ uploadToUrl, token, houseBillNo }) => {
 	// });
 
 	const { b64str, api_status_code, Res } = await callWtRestApi(houseBillNo);
+	console.log("🚀 ~ file: shippeo_pod_upload_doc.js:165 ~ uploadDocs ~ b64str:", b64str)
 	const filePath = `/tmp/${houseBillNo}.pdf`;
-	// createDirectoryIfNotExists(tempPath);
-	// const filePath = path.join(__dirname, `${houseBillNo}.pdf`);
-	// const filePath = path.join(tempPath, `${houseBillNo}.pdf`);
-	// await convertBase64ToPdf(b64str, filePath);
 	const fileData = Buffer.from(b64str, "base64");
 	console.log(
-		"🚀 ~ file: shippeo_pod_upload_doc.js:169 ~ uploadDocs ~ fileData:",
+		"🚀 ~ file: shippeo_pod_upload_doc.js:169 ~ uploadDocs ~ filePath:",
 		filePath
 	);
+
 	fs.writeFileSync(filePath, fileData);
 
 	const fileStat = fs.statSync(filePath);
 	const contentLength = String(get(fileStat, "size", 0));
 
-	const resp = await callExternalApiToUploadDoc({ filePath, fileSize: contentLength });
-	console.log("🚀 ~ file: shippeo_pod_upload_doc.js:181 ~ uploadDocs ~ resp:", resp)
+	const resp = await callExternalApiToUploadDoc({
+		filePath,
+		fileSize: contentLength,
+		token,
+		url: uploadToUrl,
+	});
 
 	// const file = fs.createReadStream(filePath, {
 	// 	encoding: "base64",
@@ -195,52 +197,27 @@ const uploadDocs = async ({ uploadToUrl, token, houseBillNo }) => {
 	// 	"🚀 ~ file: shippeo_pod_upload_doc.js:173 ~ uploadDocs ~ contentLength:",
 	// 	contentLength
 	// );
-	// try {
-	// 	const config = {
-	// 		method: "post",
-	// 		maxBodyLength: Infinity,
-	// 		url: uploadToUrl,
-	// 		headers: {
-	// 			...formData.getHeaders(),
-	// 			maxBodyLength: Infinity,
-	// 			Authorization: `Bearer ${token}`,
-	// 			"Content-Type": "multipart/form-data",
-	// 			"Content-Length": contentLength,
-	// 		},
-	// 		data: formData,
-	// 	};
-	// 	console.log(
-	// 		"🚀 ~ file: shippeo_pod_upload_doc.js:188 ~ uploadDocs ~ config:",
-	// 		JSON.stringify(config)
-	// 	);
-	// 	const response = await axios.request(config);
-	// 	// console.log(
-	// 	// 	"🚀 ~ file: shippeo_pod_upload_doc.js:182 ~ uploadDocs ~ response:",
-	// 	// 	response
-	// 	// );
-	// 	console.log("Response Data:", response.data);
-	// 	console.log("Status Code:", response.status);
-	// } catch (error) {
-	// 	if (error.response) {
-	// 		// The request was made and the server responded with a non-2xx status code
-	// 		console.log("Status Code:", error.response.status);
-	// 		console.log("Response Data:", JSON.stringify(error.response.data)); // This contains the error response data
-	// 		console.log("Response Headers:", error.response.headers);
-	// 	} else if (error.request) {
-	// 		// The request was made but no response was received
-	// 		console.error("No response received:", error.request);
-	// 	} else {
-	// 		// An error occurred while setting up the request
-	// 		console.error("Error:", error.message);
-	// 	}
-	// }
-	// if(data.status === 200){
-	// 	await insertLog();
-	// }
 
-	// docs.forEach(async (doc) => {
-	// 	fs.unlinkSync(get(doc, "filename"));
-	// });
+	if (resp.status === 200) {
+		await insertLog({housebillNumber: houseBillNo, data: resp.data});
+	}
+};
+
+const insertLog = async ({ housebillNumber, data }) => {
+	const params = {
+		TableName: LOG_TABLE,
+		Item: {
+			pKey: housebillNumber,
+			data,
+		},
+	};
+
+	try {
+		await dynamoDB.put(params).promise();
+		console.log("Item inserted successfully");
+	} catch (err) {
+		console.error("Unable to insert item. Error:", err);
+	}
 };
 
 function createDirectoryIfNotExists(directoryPath) {
@@ -269,7 +246,7 @@ async function callWtRestApi(housebill) {
 	try {
 		const url = `${process.env.WT_WEBSLI_API_URL}/9980f7b9eaffb71ce2f86734dae062/housebill=${housebill}/doctype=HOUSEBILL`;
 
-		const response = await axios.get(url);
+				const response = await axios.get(url);
 		return {
 			b64str: response.data.wtDocs.wtDoc[0].b64str,
 			api_status_code: response.status,
@@ -284,67 +261,58 @@ async function callWtRestApi(housebill) {
 	}
 }
 
-const callExternalApiToUploadDoc = async ({ filePath, fileSize }) => {
-	// return new Promise((resolve, reject) => {
-	// 	let options = {
-	// 		method: "POST",
-	// 		hostname: "api-edi.shippeo.com",
-	// 		path: "/api/orders/EDIReference/6986204/files",
-	// 		headers: {
-	// 			Authorization:
-	// 				"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTMzODMwNzYsImV4cCI6MTY5NDU5MjY3NiwiaGFzaGlkIjoiOTJFOTNNRTIifQ.aHjgHl8ZlNR1Qg7diFWu27ECoZSJ3hzN5WvwvvOQx2eu7vdVk_W5n4R11m0XR9NeUMVAIF-8yvHnPLFhnZ1eN25h7HwNcPNN_IJLAU874S-X4eGBt44_uJS19jMnG0NBZAwIVw1BlgPVGSUtYxb0U90P3usqoPzQU89gjiivXSpeny3oa40WLMBq_iSl9aCKAZHtwfF-O0eAWKxNJUyxD26r-NM0OBPOqdHm0JHUTFeeOlCYfqSufzSGTJ72NiWTW6PVj0sekTKv445AjC7NipJnq_JiIBUbwltq9DwRg4sXT8II8bEi9sSeYKXR9y_ERQSkSSUK_ITJ3uHXNzoPJQ3BOui8TRRE8f30Mo9IfzgFuB7CSMAf6xHxFaaidvznqWDg6ECUqZfhLDuKTJZ0DG773OgMCBoBehawwURRO9qrl6ObwTIHHnbUzP3q9yjOK6GxNfvW7rEb8dHG3sOtCtCcLUBVoml9UQ6tc5TffYXQYUVx4xmwfTJcFV9yk7CTDDtgfCFn32dP5fwc6bvoxKKdWPeV6XwKRfeXs-GgXe_1RldQ0i-wvKIpAptlWheqYUUyVD7IItnDzJ_9D5OKwKxPrACebkFWYhbzLXS4IhcxWp8-BOMvzZrk3rikOCiI9V57SICQqN5pAYQr8jA-fLrTsLR9ldu4_PUNuzN3Jng",
-	// 			"Content-Length": fileSize,
-	// 		},
-	// 		maxRedirects: 20,
-	// 	};
-
-	// 	let req = http.request(options, function (res) {
-	// 		let chunks = [];
-
-	// 		res.on("data", function (chunk) {
-	// 			chunks.push(chunk);
-	// 		});
-
-	// 		res.on("end", function (chunk) {
-	// 			let body = Buffer.concat(chunks);
-	// 			console.log(body.toString());
-	// 		});
-
-	// 		res.on("error", function (error) {
-	// 			console.error(error);
-	// 			reject(error);
-	// 		});
-	// 	});
-
-	// 	let postData =
-	// 		'------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name="attachments[]"; filename="/D:/tmp/6466813.pdf"\r\nContent-Type: "{Insert_File_Content_Type}"\r\n\r\n' +
-	// 		fs.readFileSync(filePath) +
-	// 		"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--";
-
-	// 	req.setHeader(
-	// 		"content-type",
-	// 		"multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
-	// 	);
-
-	// 	req.write(postData);
-
-	// 	req.end();
-	// 	resolve();
-	// });
-	return new Promise((resolve, reject) => {
-		const curlCommand = `curl --location http://api-edi.shippeo.com/api/orders/EDIReference/6986204/files --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2OTMzODMwNzYsImV4cCI6MTY5NDU5MjY3NiwiaGFzaGlkIjoiOTJFOTNNRTIifQ.aHjgHl8ZlNR1Qg7diFWu27ECoZSJ3hzN5WvwvvOQx2eu7vdVk_W5n4R11m0XR9NeUMVAIF-8yvHnPLFhnZ1eN25h7HwNcPNN_IJLAU874S-X4eGBt44_uJS19jMnG0NBZAwIVw1BlgPVGSUtYxb0U90P3usqoPzQU89gjiivXSpeny3oa40WLMBq_iSl9aCKAZHtwfF-O0eAWKxNJUyxD26r-NM0OBPOqdHm0JHUTFeeOlCYfqSufzSGTJ72NiWTW6PVj0sekTKv445AjC7NipJnq_JiIBUbwltq9DwRg4sXT8II8bEi9sSeYKXR9y_ERQSkSSUK_ITJ3uHXNzoPJQ3BOui8TRRE8f30Mo9IfzgFuB7CSMAf6xHxFaaidvznqWDg6ECUqZfhLDuKTJZ0DG773OgMCBoBehawwURRO9qrl6ObwTIHHnbUzP3q9yjOK6GxNfvW7rEb8dHG3sOtCtCcLUBVoml9UQ6tc5TffYXQYUVx4xmwfTJcFV9yk7CTDDtgfCFn32dP5fwc6bvoxKKdWPeV6XwKRfeXs-GgXe_1RldQ0i-wvKIpAptlWheqYUUyVD7IItnDzJ_9D5OKwKxPrACebkFWYhbzLXS4IhcxWp8-BOMvzZrk3rikOCiI9V57SICQqN5pAYQr8jA-fLrTsLR9ldu4_PUNuzN3Jng' --form 'attachments[]=@"${filePath}"'`;
-
-		exec(curlCommand, (error, stdout, stderr) => {
-			if (error) {
-				console.error(`Error executing cURL command: ${error}`);
-				reject(error);
-				return;
-			}
-			// Handle the output
-			console.log("cURL command output:", stdout);
-			resolve(stdout);
-		});
-	});
+const callExternalApiToUploadDoc = async ({
+	filePath,
+	fileSize,
+	token,
+	url,
+}) => {
+	const formData = new FormData();
+	formData.append(
+		"attachments[]",
+		fs.createReadStream(filePath, {
+			encoding: "base64",
+			autoClose: true,
+			start: 0,
+			end: Number(fileSize),
+		})
+	);
+	try {
+		const config = {
+			method: "post",
+			maxBodyLength: Infinity,
+			url: url,
+			headers: {
+				...formData.getHeaders(),
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "multipart/form-data",
+				"Content-Length": fileSize,
+			},
+			data: formData,
+		};
+		console.log(
+			"🚀 ~ file: shippeo_pod_upload_doc.js:188 ~ uploadDocs ~ config:",
+			JSON.stringify(config)
+		);
+		const response = await axios.request(config);
+		console.log("Response Data:", response.data);
+		console.log("Status Code:", response.status);
+		return { data: response.data, status: response.status };
+	} catch (error) {
+		if (error.response) {
+			// The request was made and the server responded with a non-2xx status code
+			console.log("Status Code:", error.response.status);
+			console.log("Response Data:", JSON.stringify(error.response.data)); // This contains the error response data
+			console.log("Response Headers:", error.response.headers);
+		} else if (error.request) {
+			// The request was made but no response was received
+			console.error("No response received:", error.request);
+		} else {
+			// An error occurred while setting up the request
+			console.error("Error:", error.message);
+		}
+		return false;
+	}
 };
 
 // const
