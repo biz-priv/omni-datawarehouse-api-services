2ac1f428d0aa0eea5805f72b62ca1f68741aab93
Madhava mvs <VenkataSaiMadhava_Matta@neimanmarcus.com>
2023-10-06 00:30:19 +0530

    removed all the python functions to basic return

--- v202/src/pod.py
+++ v202/src/pod.py
@@ -1,31 +1,5 @@
-import os
+# This function is not exist in v202
 import json
-import requests
-import logging
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.INFO)
-
-def handler(event, context):
-    LOGGER.info("Event: %s", json.dumps(event))
-    try :
-        if "file_nbr" in event["query"]:
-            num = event["query"]["file_nbr"]
-            url = os.environ["pod_URL"]+os.environ["pod_key"]+'/'+num
-        elif "house_bill_nbr" in event["query"]:
-            num = event["query"]["house_bill_nbr"]
-            url = os.environ["pod_URL"]+os.environ["pod_key"]+'/'+num+'/hawb'
-        req = requests.get(url)
-    except Exception as handler_error:
-        logging.exception("HandlerError: %s", handler_error)
-        raise HandlerError(json.dumps({"httpStatus": 501, "message": "Internal Error."})) from handler_error
-
-    if req.json()["hcpod"]["File Number"] == "ERROR":
-        raise WtPODApiError(json.dumps({"httpStatus": 400, "message": "World Track POD API Error."}))
-    response = req.content
-    LOGGER.info("Response: %s",response)
-    return response
-
-class HandlerError(Exception):
-    pass
-class WtPODApiError(Exception):
-    pass
+def handler(event, context):  # NOSONAR
+    return json.dumps(
+        {"httpStatus": 404, "message": "This function is not exist in v202"})
