36e87a2f10df4571d212e0c745d82f8255a4a24f
Jahir <jahir.uddin@bizcloudexperts.com>
2023-10-11 22:20:12 +0530

    dev done for amazon pod

--- src/pod_doc_upload.py
+++ src/pod_doc_upload.py
@@ -33,6 +33,7 @@ TOKEN_VALIDATION_TABLE_INDEX = os.environ["TOKEN_VALIDATION_TABLE_INDEX"]
 USERNAME = os.environ["AMAZON_USER_NAME"]
 PASSWORD = os.environ["AMAZON_PASSWORD"]
 TRANSACTION_TABLE = os.environ["TRANSACTION_TABLE"]
+ENVIRONMENT = os.environ["STAGE"]
 
 HOST = os.environ["HRPSL_HOST"]
 STAGE = os.environ["HRPSL_STAGE"]
@@ -48,6 +49,8 @@ COGNITO_PROVIDER = f'cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USER_PO
 
 def handler(event, context):
     body = ""
+    order_no = ""
+    housebill_no = ""
     try:
         LOGGER.info("context: %s", context)
         LOGGER.info("Event: %s", event)
@@ -73,183 +76,160 @@ def handler(event, context):
             process_amazon(order_no, housebill_no,
                            user_id, file_header_table_data, websli_token)
 
+        update_transaction_table(order_no, housebill_no, 'SUCCESS')
     except Exception as e:
-        logging.error(f"Error: {e}")
+        LOGGER.error(f"Error: {e}")
         try:
-
+            update_transaction_table(order_no, housebill_no, 'FAILED')
+            insert_log(housebill_no, body, str(e))
             sqs.send_message(
                 QueueUrl=SHIPMENT_HEADER_TABLE_STREAM_QLQ,
                 MessageBody=json.dumps(body)
             )
-            return {
-                'statusCode': 500
-            }
-            sns.publish(
-                TopicArn=SNS_TOPIC_ARN,
-                Subject="Error on shippeo-pod-upload-doc lambda",
-                Message=f"Error in shippeo-pod-upload-doc lambda: {e}"
-            )
+            if ENVIRONMENT == 'prod':
+                sns.publish(
+                    TopicArn=SNS_TOPIC_ARN,
+                    Subject="Error on pod-upload-doc lambda",
+                    Message=f"Error in pod-upload-doc lambda: {e}"
+                )
         except Exception as e:
-            logging.error(f"Error sending error notifications: {e}")
+            LOGGER.error(f"Error sending error notifications: {e}")
 
 
 def process_shippeo(order_no, housebill_no, websli_token, file_header_table_data):
-    existing_token = get_existing_token()
+    try:
+        existing_token = get_existing_token()
 
-    if existing_token == None:
-        # Get token
-        basic_auth = get_basic_auth(SHIPPEO_USERNAME, SHIPPEO_PASSWORD)
-        LOGGER.info("basic_auth: %s", basic_auth)
-        token_response = requests.post(
-            SHIPPEO_GET_TOKEN_URL,
-            headers={'Authorization': f'{basic_auth}'}
-        )
-        token = token_response.json()['data']['token']
-        existing_token = token
-        insert_token(token)
+        if existing_token == None:
+            # Get token
+            basic_auth = get_basic_auth(SHIPPEO_USERNAME, SHIPPEO_PASSWORD)
+            LOGGER.info("basic_auth: %s", basic_auth)
+            token_response = requests.post(
+                SHIPPEO_GET_TOKEN_URL,
+                headers={'Authorization': f'{basic_auth}'}
+            )
+            token = token_response.json()['data']['token']
+            existing_token = token
+            insert_token(token)
 
-    final_token = existing_token
-    LOGGER.info("websli_token: %s", websli_token)
-    # for record in records:
-    doc_type = file_header_table_data['FK_DocType']
-    LOGGER.info("doc_type: %s", doc_type)
+        final_token = existing_token
+        LOGGER.info("websli_token: %s", websli_token)
+        # for record in records:
+        doc_type = file_header_table_data['FK_DocType']
+        LOGGER.info("doc_type: %s", doc_type)
 
-    if doc_type == None:
-        LOGGER.info(
-            f"House bill : {housebill_no} with order no: {order_no} is not valid")
+        if doc_type == None:
+            LOGGER.info(
+                f"House bill : {housebill_no} with order no: {order_no} is not valid")
+            return {
+                'statusCode': 200
+            }
+
+        upload_to_url = f"{SHIPPEO_UPLOAD_DOC_URL}/{housebill_no}/files"
+        # Upload docs
+        result = upload_docs(upload_to_url, final_token,
+                             housebill_no, websli_token, doc_type)
+        if result['status'] == 200:
+            insert_log(housebill_no, result['data'])
+            update_transaction_table(order_no, housebill_no, 'SUCCESS')
         return {
             'statusCode': 200
         }
-
-    upload_to_url = f"{SHIPPEO_UPLOAD_DOC_URL}/{housebill_no}/files"
-    # Upload docs
-    result = upload_docs(upload_to_url, final_token,
-                         housebill_no, websli_token, doc_type)
-    if result['status'] == 200:
-        insert_log(housebill_no, result['data'])
-    return {
-        'statusCode': 200
-    }
+    except Exception as e:
+        LOGGER.error(f"process shippeo error: {e}")
+        raise e
 
 
 def process_amazon(order_no, housebill_no, user_id, shipment_file_data, websli_token):
-    b64_data = ""
-    doc_type = shipment_file_data['FK_DocType']
-    b64str_response = call_wt_rest_api(housebill_no, websli_token, doc_type)
-    if b64str_response == 'error':
-        LOGGER.info('Error calling WT REST API')
-        # handle error case
-    else:
-        b64_data = b64str_response['b64str']
+    try:
+        b64_data = ""
+        doc_type = shipment_file_data['FK_DocType']
+        b64str_response = call_wt_rest_api(
+            housebill_no, websli_token, doc_type)
+        if b64str_response == 'error':
+            LOGGER.info('Error calling WT REST API')
+        else:
+            b64_data = b64str_response['b64str']
 
-    if shipment_file_data == None:
-        LOGGER.info(
-            f"House bill : {housebill_no} with order no: {order_no} is not valid")
-        return {
-            'statusCode': 404
+        if shipment_file_data == None:
+            LOGGER.info(
+                f"House bill : {housebill_no} with order no: {order_no} is not valid")
+            return f"House bill : {housebill_no} with order no: {order_no} is not valid"
+
+        reference_no = get_data_from_reference_table(order_no)
+        LOGGER.info("reference_no: %s", reference_no)
+
+        pro_number = reference_no if reference_no != None else housebill_no
+        filename = shipment_file_data['FileName']
+        file_type = 'POD'
+        description = 'HCPOD'
+        file_extension = filename.split(
+            '.')[-1].lower() if isinstance(filename.split('.')[-1], str) else '.pdf'
+        mime_type = 'application/pdf' if file_extension == 'pdf' else 'image/jpeg'
+        request_source = 'OMNG'
+        location_id = 'US'
+
+        payload = {
+            "proNumber": pro_number,
+            "filename": filename,
+            "type": file_type,
+            "description": description,
+            "mimeType": mime_type,
+            "requestSource": request_source,
+            "locationId": location_id,
+            "userId": user_id
         }
+        LOGGER.info("payload: %s", payload)
+        LOGGER.info("USERNAME: %s", USERNAME)
+        LOGGER.info("PASSWORD: %s", PASSWORD)
+        LOGGER.info("COGNITO_USER_POOL_ID: %s", COGNITO_USER_POOL_ID)
+        LOGGER.info("COGNITO_IDENTITY_POOL_ID: %s", COGNITO_IDENTITY_POOL_ID)
+        LOGGER.info("COGNITO_CLIENT_ID: %s", COGNITO_CLIENT_ID)
+        LOGGER.info("COGNITO_PROVIDER: %s", COGNITO_PROVIDER)
+        LOGGER.info("COGNITO_REGION: %s", COGNITO_REGION)
+        LOGGER.info("SERVICE: %s", SERVICE)
+        LOGGER.info("REGION: %s", REGION)
 
-    reference_no = get_data_from_reference_table(order_no)
-    LOGGER.info("reference_no: %s", reference_no)
+        cognito_auth = CognitoAuth(username=USERNAME, password=PASSWORD, pool_id=COGNITO_USER_POOL_ID, identity_pool_id=COGNITO_IDENTITY_POOL_ID,
+                                   client_id=COGNITO_CLIENT_ID, provider=COGNITO_PROVIDER, pool_region=COGNITO_REGION,
+                                   service=SERVICE, service_region=REGION)
+        auth = cognito_auth.get_auth()
 
-    pro_number = reference_no if reference_no != None else housebill_no
-    filename = shipment_file_data['FileName']
-    file_type = 'POD'
-    description = 'HCPOD'
-    file_extension = filename.split('.')[-1].lower()
-    mime_type = 'application/pdf' if file_extension == 'pdf' else 'image/jpeg'
-    request_source = 'OMNG'
-    location_id = 'US'
+        LOGGER.info(
+            f'Endpoint: {ENDPOINT}, Host: {HOST}, region: {REGION}, service: {SERVICE}')
 
-    # payload = {
-    #     "pro_number": pro_number,
-    #     "filename": filename,
-    #     "file_type": file_type,
-    #     "description": description,
-    #     "mime_type": mime_type,
-    #     "request_source": request_source,
-    #     "user_id": user_id,
-    #     "location_id": location_id,
-    #     # "b64_data": b64_data,
-    #     # "user_name": AMAZON_USER_NAME,
-    #     # "password": AMAZON_PASSWORD
-    # }
+        response = requests.post(
+            ENDPOINT,
+            json=payload,
+            auth=auth
+        )
+        LOGGER.info(f"{response.status_code} Reason: {response.reason}")
 
-    payload = {
-        "proNumber": pro_number,
-        "filename": filename,
-        "type": file_type,
-        "description": description,
-        "mimeType": mime_type,
-        "requestSource": request_source,
-        "locationId": location_id,
-        "userId": user_id
-    }
-    LOGGER.info("payload: %s", payload)
-
-    save_file_to_local(f"{filename}", b64_data)
-
-    LOGGER.info("USERNAME: %s", USERNAME)
-    LOGGER.info("PASSWORD: %s", PASSWORD)
-    LOGGER.info("COGNITO_USER_POOL_ID: %s", COGNITO_USER_POOL_ID)
-    LOGGER.info("COGNITO_IDENTITY_POOL_ID: %s", COGNITO_IDENTITY_POOL_ID)
-    LOGGER.info("COGNITO_CLIENT_ID: %s", COGNITO_CLIENT_ID)
-    LOGGER.info("COGNITO_PROVIDER: %s", COGNITO_PROVIDER)
-    LOGGER.info("COGNITO_REGION: %s", COGNITO_REGION)
-    LOGGER.info("SERVICE: %s", SERVICE)
-    LOGGER.info("REGION: %s", REGION)
-
-    # return
-    cognito_auth = CognitoAuth(username=USERNAME, password=PASSWORD, pool_id=COGNITO_USER_POOL_ID, identity_pool_id=COGNITO_IDENTITY_POOL_ID,
-                               client_id=COGNITO_CLIENT_ID, provider=COGNITO_PROVIDER, pool_region=COGNITO_REGION,
-                               service=SERVICE, service_region=REGION)
-    auth = cognito_auth.get_auth()
-    LOGGER.info("auth: %s", auth)
-
-    LOGGER.info(
-        f'Endpoint: {ENDPOINT}, Host: {HOST}, region: {REGION}, service: {SERVICE}')
-
-    response = requests.post(
-        ENDPOINT,
-        json=payload,
-        auth=auth
-    )
-    LOGGER.info(f"{response.status_code} Reason: {response.reason}")
-
-    if response.ok:
-        urlJson = json.loads(response.text)
-        LOGGER.info(f"PreSigned URL: {urlJson['url']}")
-        # upload_file_to_s3(urlJson['url'], b64_data)
-        upload_file_to_s3(urlJson['url'], filename)
-        return {"statusCode": 200, "presignedUrl": urlJson['url']}
-    else:
-        raise requests.exceptions.HTTPError(
-            f"{response.status_code} Error: {response.reason} for url: {response.url} -- {response.text}")
-
-
-def save_file_to_local(file_name, base64Data):
-    binary_data = base64.b64decode(base64Data)
-    with open(file_name, "wb") as file:
-        # Write the binary data to the file
-        file.write(binary_data)
+        if response.ok:
+            url_json = json.loads(response.text)
+            LOGGER.info(f"PreSigned URL: {url_json['url']}")
+            upload_file_to_s3(url_json['url'], b64_data)
+            insert_log(housebill_no, "File uploaded to s3 successfully.")
+            return {"statusCode": 200, "data": "File uploaded to s3 successfully."}
+        else:
+            raise requests.exceptions.HTTPError(
+                f"{response.status_code} Error: {response.reason} for url: {response.url} -- {response.text}")
+    except Exception as e:
+        LOGGER.error(f"process amazon error: {e}")
+        raise e
 
 
 def upload_file_to_s3(presigned_url, file_path):
     try:
         headers = {"x-amz-server-side-encryption": "AES256",
-                   "Content-Type": "application/pdf"}
-        # LOGGER.info("file_path: %s", file_path)
-        # with open(file_path, 'rb') as file:
-        data = open(file_path, "rb")
-        LOGGER.info("data: %s", data)
-        files = {'file': (file_path, data)}
-        # pdf_binary_data = base64.b64decode(file_path)
+                   "Content-Type": "application/octet-stream"}
+        while len(file_path) % 4 != 0:
+            file_path += '='
         file_data = io.BytesIO(base64.b64decode(file_path))
-        response = requests.post(presigned_url, headers=headers, files=files)
+        response = requests.put(presigned_url, headers=headers, data=file_data)
         if response.status_code == 200:
-            urlJson = json.loads(response.text)
+            LOGGER.info("Response Content: %s", response.content)
             LOGGER.info("File uploaded successfully.")
-            LOGGER.info("urlJson: %s", urlJson)
         else:
             LOGGER.info(
                 f"Failed to upload file. Status code: {response.status_code}")
@@ -257,7 +237,7 @@ def upload_file_to_s3(presigned_url, file_path):
                 f"Failed to upload file. Status code: {response.text}")
     except Exception as e:
         LOGGER.error(f"Upload document error: {e}")
-        return {'status': 400}
+        raise e
 
 
 def get_data_from_reference_table(order_no):
@@ -285,6 +265,7 @@ def get_data_from_reference_table(order_no):
             return None
     except Exception as e:
         LOGGER.info(f"Unable to insert item. Error: {e}")
+        raise e
 
 
 def get_existing_token():
@@ -307,9 +288,11 @@ def get_existing_token():
 
     except Exception as e:
         LOGGER.info(f"Unable to insert item. Error: {e}")
+        raise e
 
 
 def insert_token(data):
+    LOGGER.info(f"TOKEN_EXPIRATION_DAYS: {TOKEN_EXPIRATION_DAYS}")
     two_days_from_now = datetime.utcnow() + timedelta(days=float(TOKEN_EXPIRATION_DAYS))
     two_days_from_now_iso = two_days_from_now.isoformat()
     params = {
@@ -323,9 +306,10 @@ def insert_token(data):
 
     try:
         dynamodb.put_item(**params)
-        LOGGER.info("Item inserted successfully")
+        LOGGER.info("Token inserted successfully")
     except Exception as e:
         LOGGER.info(f"Unable to insert item. Error: {e}")
+        raise e
 
 
 def get_websli_token(api_key):
@@ -349,6 +333,7 @@ def get_websli_token(api_key):
 
     except Exception as e:
         LOGGER.info(f"Unable to insert item. Error: {e}")
+        raise e
 
 
 def get_basic_auth(username, password):
@@ -388,11 +373,14 @@ def upload_docs(upload_to_url, token, house_bill_no, websli_token, doc_type):
         # Print response data and status code
         LOGGER.info(f"Response Data: {response.text}")
         LOGGER.info(f"Status Code: {response.status_code}")
-
-        return {'data': response.text, 'status': response.status_code}
+        if response.status_code == 200:
+            return {'data': response.text, 'status': response.status_code}
+        else:
+            raise requests.HTTPError(
+                f"Failed to upload file. Status code: {response.status_code}")
     except Exception as e:
-        logging.error(f"Upload document error: {e}")
-        return {'status': 400}
+        LOGGER.error(f"Upload document error: {e}")
+        raise e
 
 
 def call_wt_rest_api(housebill, websli_token, doc_type):
@@ -415,56 +403,58 @@ def call_wt_rest_api(housebill, websli_token, doc_type):
             raise GetDocumentError(
                 f"Error calling WT REST API for housebill {housebill}. Status Code: {response.status_code}")
 
-    except Exception as error:
+    except Exception as e:
         LOGGER.info(
-            f"Error calling WT REST API for housebill {housebill}:{error}")
-        return 'error'
+            f"Error calling WT REST API for housebill {housebill}:{e}")
+        raise e
 
 
-def insert_log(housebill_number, data):
+def insert_log(housebill_number, data, error=""):
     now = datetime.utcnow().isoformat()
     params = {
         'TableName': LOG_TABLE,
         'Item': {
             'pKey': {'S': housebill_number},
             'data': {'S': json.dumps(data)},
+            'error': {'S': error},
             'lastUpdateTime': {'S': now},
         }
     }
 
     try:
         dynamodb.put_item(**params)
-        LOGGER.info("Item inserted successfully")
+        LOGGER.info("Log inserted.")
     except Exception as e:
-        LOGGER.info(f"Unable to insert item. Error: {e}")
+        LOGGER.info(f"Unable to store token. Error: {e}")
+        raise e
 
 
-def get_if_house_bill_number_valid(order_no):
+def update_transaction_table(order_number, house_bill_number, status):
     try:
-        # Define the query parameters
-        params = {
-            'TableName': SHIPMENT_FILE_TABLE,
-            'KeyConditionExpression': "FK_OrderNo = :FK_OrderNo",
-            'FilterExpression': "(FK_DocType = :FK_DocType1 OR FK_DocType = :FK_DocType2) AND CustomerAccess = :CustomerAccess",
-            'ExpressionAttributeValues': {
-                ":FK_OrderNo": {'S': order_no},
-                ":FK_DocType1": {'S': "HCPOD"},
-                ":FK_DocType2": {'S': "POD"},
-                ":CustomerAccess": {'S': "Y"},
-            },
+        now = datetime.utcnow().isoformat()
+        key = {'orderNumber': {'S': order_number},
+               'houseBillNumber': {'S': house_bill_number}}
+        update_expression = 'SET #status = :status, #lastUpdateTime = :lastUpdateTime'
+        expression_attribute_values = {
+            ':status': {'S': status},
+            ':lastUpdateTime': {'S': now},
         }
-
-        # Query the DynamoDB table
-        response = dynamodb.query(**params)
-        LOGGER.info(f"response: {response}")
-        # Check if there are items in the result
-        items = response['Items']
-        if len(items) > 0:
-            return items[0]['FK_DocType']['S']
-        else:
-            return None
+        expression_attribute_names = {
+            '#status': 'status',
+            '#lastUpdateTime': 'lastUpdateTime'
+        }
+        response = dynamodb.update_item(
+            TableName=TRANSACTION_TABLE,
+            Key=key,
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute_values,
+            ExpressionAttributeNames=expression_attribute_names,
+            ReturnValues='UPDATED_NEW'
+        )
+        LOGGER.info(f"Transaction table update succeeded: {response}")
     except Exception as e:
-        LOGGER.info(f"Unable to insert item. Error: {e}")
+        LOGGER.error(f"Error updating item: {e}")
+        raise e
 
 
 class GetDocumentError(Exception):
