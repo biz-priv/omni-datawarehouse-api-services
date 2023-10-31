497ec7916d940bd1cc9a49aa88e216de8150285f
jahir-biz <jahir.uddin@bizcloudexperts.com>
2023-10-31 17:12:14 +0530

    response from function

--- v1/src/ltl_rating.js
+++ v1/src/ltl_rating.js
@@ -1,3 +1,8 @@
 module.exports.handler = async (event, context) => {
     console.log(`🙂 -> file: ltl_rating.js:2 -> event:`, event);
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({ hello: "world" }),
+    };
+    return response;
 };
