4c198485ae6ffdc81990495baff6b3f2acafe252
jahir-biz <jahir.uddin@bizcloudexperts.com>
2023-11-16 14:04:44 +0530

    batch rating chnage

--- v1/src/ltl_batch_rating.js
+++ v1/src/ltl_batch_rating.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { get } = require("lodash");
 const xlsx = require("xlsx");
+const { utils } = require("xlsx");
 
 module.exports.handler = async (event, context) => {
     try {
@@ -9,17 +10,25 @@ module.exports.handler = async (event, context) => {
         body = get(event, "body", "");
         if (typeof body === "string") body = JSON.parse(body);
         const fileBase64 = get(body, "data");
-        console.info(`🙂 -> file: ltl_batch_rating.js:11 -> fileBase64:`, fileBase64);
+        console.info(
+            `🙂 -> file: ltl_batch_rating.js:11 -> fileBase64:`,
+            fileBase64
+        );
         const filePath = "/tmp/output.xslx";
         const file = base64ToXlsx(fileBase64, filePath);
         const isXlsx = isValidXlsx(filePath);
         if (!isXlsx) throw new Error("Invalid file");
         console.log(`🙂 -> file: ltl_batch_rating.js:10 -> isXlsx:`, isXlsx);
+        const Loads = get(isXlsx.Sheets, "Loads", false);
+        if (!Loads) throw new Error("Sheet 'Loads' is not present");
+        const toJSON = utils.sheet_to_json(Loads);
+        console.info(`🙂 -> file: excelTest.js:5 -> workbook:`, toJSON);
+        const ltlOnly = toJSON.filter((item) => get(item, "LTL", false));
         const response = {
             statusCode: 200,
             body: JSON.stringify({
                 message: "CSV file uploaded successfully!",
-                input: isXlsx,
+                input: ltlOnly,
             }),
         };
         return response;
