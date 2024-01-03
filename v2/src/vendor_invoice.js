6a39abe43361c9bc276d95934cd29a0d33755a7b
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-01-03 19:59:53 +0530

    updates

--- v2/src/vendor_invoice.js
+++ v2/src/vendor_invoice.js
@@ -22,10 +22,10 @@ const eventValidation = Joi.object({
 });
 
 const columnFromCode = {
-  FRT: "cost",
-  FSC: "extra",
-  TAX: "tax",
-  default: "total"
+  FRT: "Cost",
+  FSC: "Extra",
+  TAX: "Tax",
+  default: "Total"
 }
 
 let itemObj = {
@@ -104,7 +104,7 @@ module.exports.handler = async (event, context, callback) => {
         get(body, "vendorInvoiceRequest.chargeList[0].code", null) !== null ||
         get(body, "vendorInvoiceRequest.chargeList[0].charge", null) !== null
       ) {
-        updateQuery = `update dbo.tbl_shipmentapar set ${get(columnFromCode, get(body, "vendorInvoiceRequest.chargeList[0].code", "default"), "total")}=sum(${get(body,"vendorInvoiceRequest.chargeList[0].charge",0)}) where fk_orderno='${fileNumber}' and fk_vendorid='${get(body,"vendorInvoiceRequest.vendorId",null)}' and finalize<>'Y'`;
+        updateQuery = `update dbo.tbl_shipmentapar set ${get(columnFromCode, get(body, "vendorInvoiceRequest.chargeList[0].code", "default"), "Total")}=sum(${get(body,"vendorInvoiceRequest.chargeList[0].charge",0)}) where fk_orderno='${fileNumber}' and fk_vendorid='${get(body,"vendorInvoiceRequest.vendorId",null)}' and finalize<>'Y'`;
       }
     } else {
       updateQuery = `update dbo.tbl_shipmentapar set refno='${get(body,"vendorInvoiceRequest.vendorReference",null)}' where fk_orderno='${fileNumber}' and fk_vendorid='${get(body,"vendorInvoiceRequest.vendorId",null)}' and finalize<>'Y'`;
